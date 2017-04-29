e72cff4897afd0eb8343529c1a4de8003b23e289
Tobias <mad.scientist@reenet.de>
2017-04-30 00:54:44 +0200

    Adding new Fragments

--- app/src/main/java/de/tobiasreich/healthtracker/MainActivity.java
+++ app/src/main/java/de/tobiasreich/healthtracker/MainActivity.java
@@ -1,13 +1,56 @@
 package de.tobiasreich.healthtracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String DETAIL_TEXT = "DETAIL_TEXT";
+
+    private final FragmentManager fMan = getSupportFragmentManager();
+
+    private LinearLayout fragmentContainer;
+    private Button buttonA;
+    private Button buttonB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("MAIN");
         setContentView(R.layout.activity_main);
+
+        fragmentContainer = (LinearLayout) findViewById(R.id.fragmentContainer);
+        buttonA = (Button) findViewById(R.id.buttonA);
+        buttonB = (Button) findViewById(R.id.buttonB);
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToFragment(new FragmentA());
+            }
+        });
+
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToFragment(new FragmentB());
+            }
+        });
     }
+
+    private void switchToFragment(final Fragment fragment) {
+        fMan.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .addToBackStack(fragment.getClass().getSimpleName())
+            .commit();
+        fMan.executePendingTransactions();
+    }
+
 }
