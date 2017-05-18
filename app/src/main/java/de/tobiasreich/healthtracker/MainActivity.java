8e3a110f734ec06103651fa9309fbcdad13f33d4
Tobias <mad.scientist@reenet.de>
2017-05-18 19:36:58 +0200

    Adding NagigationDrawer and new Support Toolbar

--- app/src/main/java/de/tobiasreich/healthtracker/MainActivity.java
+++ app/src/main/java/de/tobiasreich/healthtracker/MainActivity.java
@@ -2,9 +2,14 @@ package de.tobiasreich.healthtracker;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,7 +22,7 @@ import de.tobiasreich.healthtracker.data.medicineList.FragmentMedicineList;
 import de.tobiasreich.healthtracker.data.prescription.FragmentPrescription;
 import de.tobiasreich.healthtracker.data.profile.settings.FragmentSettings;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String DETAIL_TEXT = "DETAIL_TEXT";
@@ -34,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
         setTitle("MAIN");
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+/*
+
         fragmentContainer = (LinearLayout) findViewById(R.id.fragmentContainer);
         buttonA = (Button) findViewById(R.id.buttonA);
         buttonB = (Button) findViewById(R.id.buttonB);
@@ -41,7 +51,15 @@ public class MainActivity extends AppCompatActivity {
 
        // buttonB.setOnClickListener(v -> switchToFragment(new FragmentB()));
         buttonB.setOnClickListener(v -> switchToFragment(new FragmentPrescription()));
+*/
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
@@ -66,12 +84,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void switchToFragment(final Fragment fragment) {
-        fMan.beginTransaction()
+       /* fMan.beginTransaction()
             .replace(R.id.fragmentContainer, fragment)
             .addToBackStack(fragment.getClass().getSimpleName())
             .commit();
-        fMan.executePendingTransactions();
+        fMan.executePendingTransactions();*/
     }
+
 
 
     @SuppressLint("WrongConstant")
@@ -91,4 +110,31 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
