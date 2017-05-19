bb06e5a76d0cf0d4c51a3d976d9c61ac59766900
Tobias <mad.scientist@reenet.de>
2017-05-19 22:56:14 +0200

    Adding Listener for NavigationDrawer for showing the own name

--- app/src/main/java/de/tobiasreich/healthtracker/MainActivity.java
+++ app/src/main/java/de/tobiasreich/healthtracker/MainActivity.java
@@ -1,6 +1,9 @@
 package de.tobiasreich.healthtracker;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -9,21 +12,28 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import de.tobiasreich.healthtracker.data.medicineList.FragmentMedicineList;
 import de.tobiasreich.healthtracker.data.myMedicine.FragmentMyMedicine;
 import de.tobiasreich.healthtracker.data.prescription.FragmentPrescription;
 import de.tobiasreich.healthtracker.data.profile.settings.FragmentSettings;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private final FragmentManager fMan = getSupportFragmentManager();
+
+    private SharedPreferences sharedPref;
+
+    private TextView navHeadderTitleTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +49,62 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
+        drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                //Called when a drawer's position changes.
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                //Called when a drawer has settled in a completely open state.
+                //The drawer is interactive at this point.
+                // If you have 2 drawers (left and right) you can distinguish
+                // them by using id of the drawerView. int id = drawerView.getId();
+                // id will be your layout's id: for example R.id.left_drawer
+                String name = sharedPref.getString(getString(R.string.preferences_user_name_key), "");
+                ((TextView) drawerView.findViewById(R.id.navHeadderTitleTV)).setText(name);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Called when a drawer has settled in a completely closed state.
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                // Called when the drawer motion state changes. The new state will be one of STATE_IDLE, STATE_DRAGGING or STATE_SETTLING.
+            }
+        });
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+
+            if (id == R.id.nav_my_profile) {
+                switchToFragment(new FragmentSettings());
+            } else if (id == R.id.nav_my_medicine) {
+                switchToFragment(new FragmentMyMedicine());
+            } else if (id == R.id.nav_prescription) {
+                switchToFragment(new FragmentPrescription());
+            } else if (id == R.id.nav_evaluation) {
+
+            } else if (id == R.id.nav_medicine_list) {
+                switchToFragment(new FragmentMedicineList());
+            }
+
+            DrawerLayout drawer1 = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer1.closeDrawer(GravityCompat.START);
+            return true;
+        });
+
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+
     }
 
     @Override
@@ -62,28 +123,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_my_profile) {
-            switchToFragment(new FragmentSettings());
-        } else if (id == R.id.nav_my_medicine) {
-            switchToFragment(new FragmentMyMedicine());
-        } else if (id == R.id.nav_prescription) {
-            switchToFragment(new FragmentPrescription());
-        } else if (id == R.id.nav_evaluation) {
-
-
-        } else if (id == R.id.nav_medicine_list) {
-            switchToFragment(new FragmentMedicineList());
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 
     private void switchToFragment(final Fragment fragment) {
         fMan.beginTransaction()
